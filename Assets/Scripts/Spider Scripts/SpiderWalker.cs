c4b208cd1c7520f28f7ddbe2da40256ce33a7f79
Vítor Luís <vitorluis.mauricio@hotmail.com>
2017-04-22 02:45:50 -0300

    Spider Walker
    Spider Walker created

---
+++ Assets/Scripts/Spider Scripts/SpiderWalker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderWalker : MonoBehaviour {
+
+	Rigidbody2D rigidBody;
+	float speed = 2f;
+	bool grounded;
+
+	[SerializeField]
+	GameObject startPos, endPos;
+
+	void Awake() {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		Move ();
+		CheckCollision ();
+	}
+
+	void Move() {
+		Vector2 temp = rigidBody.velocity;
+		temp.x = transform.localScale.x * speed;
+		rigidBody.velocity = temp;
+	}
+
+	void CheckCollision() {
+		grounded = Physics2D.Linecast(startPos.transform.position, endPos.transform.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if (!grounded) {
+			Vector3 temp = transform.localScale;
+			temp.x = -temp.x;
+			transform.localScale = temp;
+		}
+	}
+}
